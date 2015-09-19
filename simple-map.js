548a47463392d219ef9e489007d946102f7a36fb
Nick Adair <nick.adair@live.com>
2015-09-19 02:30:32 +0000

    Testing

--- simple-map.js
+++ simple-map.js
@@ -2,6 +2,7 @@ if (Meteor.isClient) {
   Template.body.helpers({
     loc: function () {
       // return 0, 0 if the location isn't ready
+      // this is another comment
       return Geolocation.latLng() || { lat: 0, lng: 0 };
     },
     error: Geolocation.error
